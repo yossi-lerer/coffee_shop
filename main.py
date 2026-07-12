83233ddd3c0fad0a32d3fd637cacbed769f8d7f0
yossi-lerer <yossi.lerer.2003@gmail.com>
2026-07-12 11:39:32 +0300

    finish step 2 - Customer Greeting

--- main.py
+++ main.py
@@ -9,3 +9,14 @@ class MenuItem:
 
 espresso = MenuItem("Espresso", 3.5)
 espresso.describe()
+# step 2 - Customer Greeting
+class Customer:
+    def __init__(self, name, favorite_drink):
+        self.name = name
+        self.favorite_drink = favorite_drink
+        
+    def greet(self):
+        print(f"Hi! I am {self.name} and I would like a {self.favorite_drink}.")
+
+customer_alice = Customer("Alice", "Latte")
+customer_alice.greet()
